a96620dcbaa3c2be3a70bab78d922167f0e5c27b
ABucket <82869104+ABucket@users.noreply.github.com>
2021-11-02 00:35:11 +0800

    Add files via upload
    fix remote config

--- Darknights-proxy.py
+++ Darknights-proxy.py
@@ -1,46 +1,58 @@
 from mitmproxy.options import Options
 from mitmproxy.tools.web.master import WebMaster
 from mitmproxy.http import HTTPFlow
+import mitmproxy.http
+
+from mitmproxy import ctx, http
+import json, time
+
 
 remote_server = "example.com"
 proxy_enable = True
+
+
 class ArkInterceptor():
     if not proxy_enable:
         Servers = {"ak-gs-localhost.hypergryph.com": ("ak-gs.hypergryph.com", 8443),
                    "ak-gs-b-localhost.hypergryph.com": ("ak-gs.hypergryph.com", 8443),
                    "ak-as-localhost.hypergryph.com": ("ak-as.hypergryph.com", 9443)
-                }
+                   }
     else:
         Servers = {"ak-gs-localhost.hypergryph.com": (remote_server, 9443),
                    "ak-gs-gf.hypergryph.com": (remote_server, 9443),
                    "ak-gs-b-localhost.hypergryph.com": (remote_server, 9443),
                    "ak-as-localhost.hypergryph.com": (remote_server, 9443),
-                   "as.hypergryph.com" : (remote_server, 9443),
-                   "bi-track.hypergryph.com" : (remote_server, 9443),
+                   "as.hypergryph.com": (remote_server, 9443),
+                   "bi-track.hypergryph.com": (remote_server, 9443),
                    "gv1.xdrig.com": (remote_server, 9443),
                    "me.xdrig.com": (remote_server, 9443),
                    "ak-fs.hypergryph.com": (remote_server, 9443)
-                }
+                   }
 
     ServersList = [key for key in Servers.keys()] + [val[0] for val in Servers.values()]
-    
+
     def __init__(self):
         print("Proxy Loaded.")
-        
+
     def http_connect(self, flow: HTTPFlow):
         if (flow.request.host in self.Servers.keys()):
             flow.request.host, flow.request.port = self.Servers.get(flow.request.host)
+            
+    def remote_config_edit(self, flow: HTTPFlow):
+        if flow.request.host == "ak-conf.hypergryph.com" and flow.request.path == "/config/prod/official/remote_config":
+            flow.response.set_text('{"enableGameBI": true, "enableSDKNetSecure" : false, "enableBestHttp": false}')
 
-addons=[
+
+addons = [
     ArkInterceptor()
 ]
 
 # run directly for localhost only
 if __name__ == "__main__":
     ops = Options(
-    listen_host='0.0.0.0', 
-    listen_port=8080,   
-    ssl_insecure=True
+        listen_host='0.0.0.0',
+        listen_port=8080,
+        ssl_insecure=True
     )
     master = WebMaster(ops)
     master.addons.add(ArkInterceptor())
