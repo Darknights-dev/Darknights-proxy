2f874805597bea3e49d07f567bbe18b8256e4760
Darknights-master <82692776+Darknights-master@users.noreply.github.com>
2022-08-23 09:09:30 +0800

    1861 update
    Additional tricks are needed to use this proxy.

--- Darknights-proxy.py
+++ Darknights-proxy.py
@@ -4,45 +4,18 @@ from mitmproxy.proxy.server import ProxyServer
 from mitmproxy.tools.web.master import WebMaster
 from mitmproxy.http import HTTPFlow
 
-remote_server = "example.com"
-proxy_enable = True
+network_config = """{"sign":"N+TjgIU1VP85wUeRVcmU6k9w3x3oTlKDXM9oK2TllRtryxTH2S9zMCAgUKIBvhinssBE7Dkll34G0llfUYdlnJTWar+OydnkEN0DA9ecWuoNdQRJ3fIAyYMDRsrTjcrkfUqDJ6GdB9MivqYBm5MKhFxzdI1UtY2kJxC9TZJR5m0=","content":"{\\"configVer\\":\\"5\\",\\"funcVer\\":\\"V030\\",\\"configs\\":{\\"V030\\":{\\"override\\":true,\\"network\\":{\\"gs\\":\\"placeholder\\",\\"as\\":\\"placeholder\\",\\"u8\\":\\"placeholder\\",\\"hu\\":\\"https://ak.hycdn.cn/assetbundle/official\\",\\"hv\\":\\"https://ak-conf.hypergryph.com/config/prod/official/{0}/version\\",\\"rc\\":\\"https://ak-conf.hypergryph.com/config/prod/official/remote_config\\",\\"an\\":\\"https://ak-conf.hypergryph.com/config/prod/announce_meta/{0}/announcement.meta.json\\",\\"prean\\":\\"https://ak-conf.hypergryph.com/config/prod/announce_meta/{0}/preannouncement.meta.json\\",\\"sl\\":\\"https://ak.hypergryph.com/protocol/service\\",\\"of\\":\\"placeholder/index.html\\",\\"pkgAd\\":\\"https://ak.hypergryph.com/download\\",\\"pkgIOS\\":\\"https://apps.apple.com/cn/app/id1454663939\\",\\"secure\\":false}}}}"}"""
 
+dist = "https://example.com"
 
 class ArkInterceptor():
-    if not proxy_enable:
-        Servers = {"ak-gs-localhost.hypergryph.com": ("ak-gs.hypergryph.com", 8443),
-                   "ak-gs-b-localhost.hypergryph.com": ("ak-gs.hypergryph.com", 8443),
-                   "ak-as-localhost.hypergryph.com": ("ak-as.hypergryph.com", 9443)
-                   }
-    else:
-        Servers = {"ak-gs-localhost.hypergryph.com": (remote_server, 9443),
-                   "ak-gs-gf.hypergryph.com": (remote_server, 9443),
-                   "ak-gs-b-localhost.hypergryph.com": (remote_server, 9443),
-                   "ak-as-localhost.hypergryph.com": (remote_server, 9443),
-                   "as.hypergryph.com": (remote_server, 9443),
-                   "bi-track.hypergryph.com": (remote_server, 9443),
-                   "gv1.xdrig.com": (remote_server, 9443),
-                   "me.xdrig.com": (remote_server, 9443),
-                   "ak-fs.hypergryph.com": (remote_server, 9443)
-                   }
-
-    ServersList = [key for key in Servers.keys()] + [val[0] for val in Servers.values()]
 
     def __init__(self):
         print("Proxy Loaded.")
 
-    def http_connect(self, flow: HTTPFlow):
-        if (flow.request.host in self.Servers.keys()):
-            flow.request.host, flow.request.port = self.Servers.get(flow.request.host)
-
     def response(self, flow: HTTPFlow):
-        if flow.request.host == "ak-conf.hypergryph.com" and flow.request.path == "/config/prod/official/remote_config":
-            flow.response.set_text('{"enableGameBI": true, "enableSDKNetSecure" : false, "enableBestHttp": false}')
-
-
-addons = [
-    ArkInterceptor()
-]
+        if not flow.request.path.find("network_config") == -1:
+            flow.response.set_text(str(network_config).replace("placeholder",dist))
 
 # run directly for localhost only
 if __name__ == "__main__":
@@ -55,3 +28,7 @@ if __name__ == "__main__":
     master.server = ProxyServer(ProxyConfig(ops))
     master.addons.add(ArkInterceptor())
     master.run()
+
+addons = [
+    ArkInterceptor()
+]
